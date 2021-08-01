f034d71082568880deca7c693cbcf7e357d3f639
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-08-01 19:31:32 +0900

    update usePromise description

--- hooks-study/src/UsePromiseSample.js
+++ hooks-study/src/UsePromiseSample.js
@@ -9,11 +9,11 @@ const wait = () => {
 };
 
 const UsePromiseSample = () => {
-  const [loading, resolved, error] = usePromise(wait, []);
+  const [loading, resolved, error] = usePromise(wait, []); //deps가 빈배열인 이유 빈배열을 넣기 때문이다.
 
-  if (loading) return <div>로딩중..!</div>;
+  if (loading) return <div>로딩중..!</div>; //로드를 하는 시간인 3초 동안은 loading이 true일것이고, 이후에는 false로 바뀌고
   if (error) return <div>에러 발생!</div>;
-  if (!resolved) return null;
+  if (!resolved) return null; //resolved가 그동안 null이기 때문에 true로 null이고 이후에 Hello hooks!가 보여질 것이다!
 
   return <div>{resolved}</div>;
 };
--- hooks-study/src/usePromise.js
+++ hooks-study/src/usePromise.js
@@ -7,21 +7,22 @@ export default function usePromise(promiseCreator, deps) {
   const [error, setError] = useState(null);
 
   const process = async () => {
-    setLoading(true);
+    setLoading(true); //true값 만들어주고,
     try {
-      const result = await promiseCreator();
-      setResolved(result);
+      const result = await promiseCreator(); //promise생성 해주고 이게 wait 펑션이된다?
+      setResolved(result); //생성된 프로미스 넣어주고
     } catch (e) {
-      setError(e);
+      setError(e); //에러처리
     }
-    setLoading(false);
+    setLoading(false); //false로 만들어주면서닫아준다.
   };
 
   useEffect(() => {
-    process();
+    process(); //처음 렌더링될때 프로미스한번만 동작하게 하기위함
+    console.log(deps);
   }, deps); //언제 프로미스를 새로만들지에 대한 deps배열이다. 기본 값은 비어있는 배열이다.
   //비어있는 배열이기 때문에 가장 처음에 렌더링 될때만 실행된다.
   //useEffect를 사용할때는 async 를 사용해서는 안된다. 뒷정리 함수를 반환해야되는데
   //async를 사용하면 함수가 아닌 프로미스를 반환하기 때문에 오류가 발생한다.
-  return [loading, resolved, error];
+  return [loading, resolved, error]; //반환 하는 값을 Sample에서 그대로 사용한다.
 }
